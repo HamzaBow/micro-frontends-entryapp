834b6778addec54ac49217ec37644b350806d874
Hamza Boukhentiche <boukhentichehamza@gmail.com>
2024-06-02 00:30:50 +0100

    refactor: format file

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "entryapp",
+      name: 'entryapp',
       remotes: {
-        service1: "http://localhost:5001/assets/remoteEntry.js",
-        service2: "http://localhost:5002/assets/remoteEntry.js",
+        service1: 'http://localhost:5001/assets/remoteEntry.js',
+        service2: 'http://localhost:5002/assets/remoteEntry.js',
       },
-      shared: ["react", "react-dom"],
+      shared: ['react', 'react-dom'],
     }),
   ],
   build: {
     modulePreload: false,
-    target: "esnext",
+    target: 'esnext',
     minify: false,
     cssCodeSplit: false,
   },
-});
+})
