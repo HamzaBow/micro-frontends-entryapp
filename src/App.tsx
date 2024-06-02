e4c638ae12a41979092c42898e486a40dbb21cf9
Hamza Boukhentiche <boukhentichehamza@gmail.com>
2024-06-02 03:28:09 +0100

    use sidebar + content from two different remote apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
-import './App.css'
 // @ts-expect-error this is a remote component (micro-frontends)
-import ButtonFromService1 from 'service1/Button'
+import Service1App from 'service1/Service1App'
 // @ts-expect-error this is a remote component (micro-frontends)
-import ButtonFromService2 from 'service2/Button'
+import Service2App from 'service2/Service2App'
+
+const serviceNameArrayConst = ['service1', 'service2'] as const
+// const serviceNameArray      = [...serviceNameArrayConst]
+type ServiceName = (typeof serviceNameArrayConst)[number]
+
+type Services = Record<ServiceName, { component: JSX.Element; title: string }>
+
+const services: Services = {
+  service1: {
+    component: Service1App,
+    title: 'Service 1',
+  },
+  service2: {
+    component: Service2App,
+    title: 'Service 2',
+  },
+}
 
 function App() {
+  const service: ServiceName = window.location.pathname.split(
+    '/'
+  )[1] as ServiceName
   return (
-    <div>
-      <div className='absolute left-0 right-0 top-0 z-10 flex h-8 items-center justify-between bg-slate-500 px-8 py-1'>
-        <div>Acme Corporation</div>
+    <div className='h-full'>
+      <div className='flex h-8 w-full items-center justify-between bg-slate-500 px-8 py-1'>
+        <div className='flex gap-2'>
+          <div className='mr-6'>Acme Corporation</div>
+          {Object.keys(services).map((serviceName) => (
+            <a
+              key={serviceName}
+              href={`/${serviceName}`}
+              className={`${serviceName === service ? 'bg-indigo-600' : ''}`}
+            >
+              {services[serviceName as ServiceName]['title']}
+            </a>
+          ))}
+        </div>
         <div>Account</div>
       </div>
-      <ButtonFromService1 />
-      <ButtonFromService2 />
+
+      {Object.keys(services).map((serviceName) => {
+        if (serviceName !== service) return null
+        const ServiceComponent = services[serviceName as ServiceName].component
+        // @ts-expect-error abcd
+        return <ServiceComponent key={serviceName} />
+      })}
     </div>
   )
 }
