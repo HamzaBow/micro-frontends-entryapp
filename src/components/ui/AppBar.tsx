f45c437cae8810fb9bcb236e73c6651142123c13
Hamza Boukhentiche <boukhentichehamza@gmail.com>
2024-06-03 09:12:57 +0100

    add some style adjustments

--- src/components/ui/AppBar.tsx
+++ src/components/ui/AppBar.tsx
@@ -18,7 +18,7 @@ export function AppBar() {
     '/'
   )[1] as ServiceName
   return (
-    <header className='bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6'>
+    <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
       <nav className='hidden flex-row gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
         <a
           href='/'
@@ -29,13 +29,13 @@ export function AppBar() {
         </a>
         <a
           href='/service1'
-          className={`${service === 'service1' ? 'text-foreground' : 'text-muted-foreground'} hover:text-foreground whitespace-nowrap transition-colors`}
+          className={`${service === 'service1' ? 'text-foreground' : 'text-muted-foreground'} whitespace-nowrap transition-colors hover:text-foreground`}
         >
           Service 1
         </a>
         <a
           href='/service2'
-          className={`${service === 'service2' ? 'text-foreground' : 'text-muted-foreground'} hover:text-foreground whitespace-nowrap transition-colors`}
+          className={`${service === 'service2' ? 'text-foreground' : 'text-muted-foreground'} whitespace-nowrap transition-colors hover:text-foreground`}
         >
           Service 2
         </a>
@@ -73,9 +73,9 @@ export function AppBar() {
       </Sheet>
 
       <div className='flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4'>
-        <form className='ml-auto flex-1 sm:flex-initial'>
+        <form className='ml-5 flex-1 sm:flex-initial'>
           <div className='relative'>
-            <Search className='text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4' />
+            <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
             <Input
               type='search'
               placeholder='Search services...'
